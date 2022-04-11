406a75956a199ba687a38035e481dc78284b2a0b
Ruan Petrus <xastroboyx11@gmail.com>
2022-04-11 17:47:40 -0300

    Adicionado os pacotes

---
+++ src/cineMarcus/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cineMarcus;
+
+/**
+ *
+ * @author ruan
+ */
+public class Main {
+    public static void main(String[] argv)
+    {
+        System.out.println("Hello World");
+    }
+}
